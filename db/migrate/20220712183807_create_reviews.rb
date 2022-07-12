42f211cdc78ea9799778fa86d4e6069775b0790e
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-12 23:27:27 +0430

    add reviews columns and reviews references books

--- db/migrate/20220712183807_create_reviews.rb
+++ db/migrate/20220712183807_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
+      t.string :author
+      t.string :comment
+      t.references :books
 
       t.timestamps
     end
